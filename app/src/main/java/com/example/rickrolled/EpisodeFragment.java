18f4ec13646bdcc770688df1f70cc091ce9bf243
Arsh150701 <45517849+Arsh150701@users.noreply.github.com>
2021-06-09 19:58:11 +0530

    added template for EpisodeFragment

---
+++ app/src/main/java/com/example/rickrolled/EpisodeFragment.java
@@ -0,0 +1,27 @@
+package com.example.rickrolled;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EpisodeFragment extends Fragment {
+
+    public EpisodeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_episode, container, false);
+    }
+}
