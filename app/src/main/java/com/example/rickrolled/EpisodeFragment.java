fddb05b9708d1894c053396a3744e5a1c208a2e7
Devansh <201851038@iiitvadodara.ac.in>
2021-06-21 14:19:13 +0530

    like option added for episode(not working)

--- app/src/main/java/com/example/rickrolled/EpisodeFragment.java
+++ app/src/main/java/com/example/rickrolled/EpisodeFragment.java
@@ -2,13 +2,18 @@ package com.example.rickrolled;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class EpisodeFragment extends Fragment {
 
@@ -24,6 +29,7 @@ public class EpisodeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -39,5 +45,11 @@ public class EpisodeFragment extends Fragment {
         episodeName.setText(data.getName());
         firstAired.setText(String.format(getResources().getString(R.string.firstAired), data.getAir_date()));
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.single_episode_menu, menu);
     }
 }
